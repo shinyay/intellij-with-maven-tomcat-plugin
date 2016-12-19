cc579d89df636e796b8370447271c0ba99168a4b
U-SYANAGIH-JP\syanagih <syanagih@SYANAGIH-JP.jp.oracle.com>
2016-12-19 15:41:16 +0900

    Modify it to remove archive file after extract

--- bin/get-tomcat.sh
+++ bin/get-tomcat.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-wget --no-check-certificate http://download.nus.edu.sg/mirror/apache/tomcat/tomcat-7/v7.0.73/bin/apache-tomcat-7.0.73.zip -O apache-tomcat-7.0.73.zip
+ARCHIVE_NAME=apache-tomcat-7.0.73.zip
+wget --no-check-certificate http://download.nus.edu.sg/mirror/apache/tomcat/tomcat-7/v7.0.73/bin/${ARCHIVE_NAME} 
+unzip ${ARCHIVE_NAME}
+rm ${ARCHIVE_NAME}
