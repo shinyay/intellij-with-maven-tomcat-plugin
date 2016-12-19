7a2565aebe8e52b14333d877423f888e74a75852
U-SYANAGIH-JP\syanagih <syanagih@SYANAGIH-JP.jp.oracle.com>
2016-12-19 15:36:41 +0900

    Add Shell which extracts WAR to webapp ROOT

---
+++ bin/deploy-app-to-tomcat.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ -z $1 ] || [ -z $2 ]; then
+	echo "usage: $0 <WAR PATH> <TOMCAT INSTALLED ROOT>"
+	exit -1
+fi
+
+WAR_PATH=$1
+TOMCAT_ROOT=`echo $2 | sed -e 's/\/$//g'`
+CURRENT_DIR=`pwd`
+
+if [ -e $TOMCAT_ROOT/webapps/ROOT ]; then
+	cd $TOMCAT_ROOT/webapps/ROOT
+else
+	mkdir $TOMCAT_ROOT/webapps/ROOT
+	cd $TOMCAT_ROOT/webapps/ROOT
+fi
+
+jar xvf $CURRENT_DIR/$WAR_PATH
